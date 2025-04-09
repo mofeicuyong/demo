f201b19f7b17b525de4d2cd205d31ed12d334895
mofeicuyong <dongzefan@cvloo.com>
2025-04-09 17:36:07 -0400

    test s3

--- djangoProject/users/tests.py
+++ djangoProject/users/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import boto3
 
-# Create your tests here.
+s3 = boto3.client('s3')
+
+# 设置参数
+bucket_name = 'stock'
+s3_key = 'apple_stock.csv'
+local_file_path = 'apple_stock.csv'
+
+# 下载文件
+s3.download_file(bucket_name, s3_key, local_file_path)
+
+print("✅ 文件下载完成！")
